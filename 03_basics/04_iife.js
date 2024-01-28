ea8da727616248ac0dbf13c2a3b3470a57f536c3
Arpit Rathore <78910306+ArpitRathore2020@users.noreply.github.com>
2024-01-28 15:49:56 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,10 +1,16 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+// IIFE have their own scope
+// used to avoid global scope pollution
 
+// this means they 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); 
+
+// semicolon is necessary otherwise the function wouldn't know where the context ends
+// the last() are for execution of the function written inside
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
